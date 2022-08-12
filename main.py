b51b3f82f4eba148d8c5673134c2bdb091da604d
Mckenzie <mckenzieturner@protonmail.com>
2022-08-12 00:35:14 -0300

    If statement has been altered

--- main.py
+++ main.py
@@ -13,22 +13,22 @@ total_amount = []
 
 
 def start_up():
-    desktop_folder = os.path.expanduser('~/Desktop/M.I.R.S')
-    directory = os.path.exists(desktop_folder)
-    file_count = len(os.listdir(desktop_folder))
+    desktop_directory = os.path.expanduser('~/Desktop/M.I.R.S')
+    directory = os.path.exists(desktop_directory)
+    file_count = len(os.listdir(desktop_directory))
     files = []
 
     try:
         if not directory:
-            os.makedirs(desktop_folder)
-            print(f"Please place the '.csv' withing {desktop_folder}")
+            os.makedirs(desktop_directory)
+            print(f"Please place the '.csv' withing {desktop_directory}")
         else:
             if file_count == 0:
-                print(f"Please place the '.csv' withing {desktop_folder}\nRestart to begin")
+                print(f"Please place the '.csv' withing {desktop_directory}\nRestart to begin")
                 return
             else:
-                for file in os.listdir(desktop_folder):
-                    files.append(desktop_folder + '/' + file)
+                for file in os.listdir(desktop_directory):
+                    files.append(desktop_directory + '/' + file)
 
     except Exception as error:
         print(error)
@@ -60,10 +60,9 @@ def data_scrape(file, month, key):
                 data_final_amount = split_data[3]
                 data_reward_coin = split_data[4]
 
-                if month in data_date:
-                    if data_reward_type == key:
-                        print(data_date + ' ' + data_reward_amount + ' ' + data_reward_coin)
-                        total_amount.append(float(data_reward_amount))
+                if month in data_date and key == data_reward_type:
+                    print(data_date + ' ' + data_reward_amount + ' ' + data_reward_coin)
+                    total_amount.append(float(data_reward_amount))
 
     print(f"\nTotal = {sum(total_amount)}" + f" {key}")
 
